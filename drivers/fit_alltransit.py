fe3228824cd4d878321d5b6be2a7eb5444443c2f
lgbouma <bouma.luke@gmail.com>
2020-12-22 17:27:51 -0500

    fit_alltransit: got it working

--- drivers/fit_alltransit.py
+++ drivers/fit_alltransit.py
@@ -1,6 +1,6 @@
 """
-Fit the TESS + groundbased TOI 1937 data for {"period", "t0", "r", "b", "u0", "u1"}.
-i.e., believe the error bars, and fix the depth.
+Fit the TESS + groundbased TOI 1937 data for {"period", "t0", "log_r", "b",
+"u0", "u1"}, plus instrument means.  Believe the error bars, and fix the depth.
 """
 
 import numpy as np, matplotlib.pyplot as plt, pandas as pd, pymc3 as pm
@@ -9,9 +9,10 @@ from collections import OrderedDict
 from pymc3.backends.tracetab import trace_to_dataframe
 
 if 'PU' in socket.gethostname():
-    # NOTE: this is a janky catalina workaround for theano compilation failure
-    import theano
-    theano.config.gcc.cxxflags = "-Wno-c++11-narrowing"
+    # NOTE: require theano.gcc.cxxflags set to "-Wno-c++11-narrowing"
+    assert os.path.exists(
+        os.path.join(os.path.expanduser('~'), '.theanorc')
+    )
 
 import exoplanet as xo
 
@@ -88,7 +89,17 @@ def fit_alltransit(starid='TOI_1937', N_samples=1000):
     datasets['tess'] = [time, flux, flux_err, tess_texp]
 
     # TODO: include the Evans r-band, 20200214
+    # 20200122_ip_TIC268301217.epdlc
+    # 20200129_ip_TIC268301217.epdlc
+    # 20200131_ip_TIC268301217.epdlc
+    # 20200213_gp_TIC268301217.epdlc
+    # 20200422_zs_TIC268301217.epdlc
     datestrs = ['20200122', '20200129', '20200131', '20200213', '20200422']
+    groundkeytobandpass = {'ground_0': r'i$_\mathrm{LCOGT}$',
+                           'ground_1': r'i$_\mathrm{LCOGT}$',
+                           'ground_2': r'i$_\mathrm{LCOGT}$',
+                           'ground_3': r'g$_\mathrm{LCOGT}$',
+                           'ground_4': r'z$_\mathrm{LCOGT}$'}
     for ix, d in enumerate(datestrs):
         x_obs, y_obs, y_err, t_exp = get_groundphot(datestr=d)
         x_obs -= 2457000 # convert to BTJD
@@ -127,21 +138,35 @@ def fit_alltransit(starid='TOI_1937', N_samples=1000):
     phaseplot = 1
     cornerplot = 1
     posttable = 1
+    grounddepth = 1
+
+    if grounddepth:
+        outpath = join(PLOTDIR, f'{starid}_{modelid}_grounddepth.png')
+        bp.plot_grounddepth(m, summdf, outpath, modelid=modelid,
+                            showerror=0,
+                            groundkeytobandpass=groundkeytobandpass,
+                            tdur=EPHEMDICT[starid]['tdur'])
+        outpath = join(PLOTDIR, f'{starid}_{modelid}_grounddepth_talkaspect.png')
+        bp.plot_grounddepth(m, summdf, outpath, modelid=modelid,
+                            showerror=0, talkaspect=1,
+                            groundkeytobandpass=groundkeytobandpass,
+                            tdur=EPHEMDICT[starid]['tdur'])
 
     if posttable:
         outpath = join(PLOTDIR, f'{starid}_{modelid}_posteriortable.tex')
         make_posterior_table(pklpath, priordict, outpath, modelid, makepdf=1)
-
-    if cornerplot:
-        outpath = join(PLOTDIR, f'{starid}_{modelid}_cornerplot.png')
-        bp.plot_cornerplot(list(priordict), m, outpath)
 
     if phaseplot:
         outpath = join(PLOTDIR, f'{starid}_{modelid}_phaseplot.png')
         bp.plot_phasefold(m, summdf, outpath, modelid=modelid, inppt=1,
                           xlim=(-2.1,2.1), binsize_minutes=5, savepdf=1)
 
+    if cornerplot:
+        outpath = join(PLOTDIR, f'{starid}_{modelid}_cornerplot.png')
+        bp.plot_cornerplot(list(priordict), m, outpath)
+
     if fitindiv:
+        raise NotImplementedError("not important for the alltransit model case")
         outpath = join(PLOTDIR, f'{starid}_{modelid}_fitindiv.png')
         bp.plot_fitindiv(m, summdf, outpath, modelid=modelid)
 
