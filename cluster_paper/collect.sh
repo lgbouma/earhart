81e68f31c1ee5eec5f4abd573f7d7d1e51679c4e
lgbouma <bouma.luke@gmail.com>
2021-01-27 12:59:49 -0500

    add words

--- cluster_paper/collect.sh
+++ cluster_paper/collect.sh
@@ -1,4 +1,3 @@
-
 cp ../results/cluster_membership/full_kinematics_bright_galactic.pdf f1.pdf
 
 cp ../results/hr/hr_Bp_m_Rp_fullfaint_edr3.pdf f2a.pdf
@@ -6,3 +5,17 @@ cp ../results/hr/hr_colorhalobyglat_Bp_m_Rp_show1937_fullfaint_edr3.pdf f2b.pdf
 
 cp ../results/rotation/NGC_2516/NGC_2516_rotation_vs_BpmRp_corehalosplit_linear.pdf f3a.pdf
 cp ../results/rotation/NGC_2516/NGC_2516_rotation_vs_BpmRp_corehalosplit_log.pdf f3b.pdf
+
+cp ../results/skypositions/skypositions_x_rotn.pdf f4.pdf
+
+cp ../results/lithium/randich_lithium_vs_BpmRp_xmatch_goldrot_corehalosplit.pdf f5a.pdf
+cp ../results/rotation_X_lithium/rotation_vs_BpmRp_X_randich18_lithium_viridis.pdf f5b.pdf
+
+# supplementary
+cp ../results/rotation_X_RUWE/rotation_vs_BpmRp_X_RUWE_viridis.pdf f6.pdf
+
+# cp ../results/calc_backintegration_ngc2516/core_halo_to_ngc2516_separation.pdf f7a.pdf
+# cp ../results/calc_backintegration_ngc2516/core_halo_to_ngc2516_separation_fix_rvs.pdf f7b.pdf
+# 
+# cp ../results/glue_ngc2516_manual_rotator_viz/slow_rotators_are_lower_declination_and_closer_to_sun.png temp0.png
+# cp ../results/glue_ngc2516_manual_rotator_viz/fast_rotators_are_spread_throughout_core_and_halo.png temp1.png
