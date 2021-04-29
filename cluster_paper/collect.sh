16bc5744149d388f707f1ef8a1b282ceb46cfde7
lgbouma <bouma.luke@gmail.com>
2021-04-29 10:42:02 -0700

    update ms

--- cluster_paper/collect.sh
+++ cluster_paper/collect.sh
@@ -1,53 +1,43 @@
-#cp ../results/cluster_membership/full_kinematics_bright_galactic.pdf f1.pdf
+# main text
 cp ../results/cluster_membership/full_kinematics_fullfaint_galactic.pdf f1.pdf
 
-cp ../results/hr/hr_Bp_m_Rp_fullfaint_edr3.pdf f2a.pdf
-cp ../results/hr/hr_parsec_Bp_m_Rp_parsec_fullfaint_edr3.pdf f2b.pdf
-cp ../results/hr/hr_colorhalobyglat_Bp_m_Rp_show1937_fullfaint_edr3.pdf f2c.pdf
+cp ../results/vtangential_projection/XYZ_with_orbit.pdf f2.pdf
 
-# cp ../results/rotation/NGC_2516/NGC_2516_rotation_vs_BpmRp_corehalosplit_linear_defaultcleaning.pdf f3a.pdf
-# cp ../results/rotation/NGC_2516/NGC_2516_rotation_vs_BpmRp_corehalosplit_linear_periodogram_match.pdf f3b.pdf
-# cp ../results/rotation/NGC_2516/NGC_2516_rotation_vs_BpmRp_emphbinaries_linear_defaultcleaning.pdf f3c.pdf
-# cp ../results/rotation/NGC_2516/NGC_2516_rotation_vs_BpmRp_emphbinaries_linear_periodogram_match.pdf f3d.pdf
+cp ../results/hr/hr_Bp_m_Rp_fullfaint_edr3.pdf f3a.pdf
+cp ../results/hr/hr_parsec_Bp_m_Rp_parsec_fullfaint_edr3.pdf f3b.pdf
 
-cp ../results/rotation/NGC_2516/NGC_2516_rotation_vs_BpmRp_corehalosplit_talkaspect_linear_defaultcleaning.pdf f3a.pdf
-cp ../results/rotation/NGC_2516/NGC_2516_rotation_vs_BpmRp_corehalosplit_talkaspect_linear_periodogram_match.pdf f3b.pdf
-cp ../results/rotation/NGC_2516/NGC_2516_rotation_vs_BpmRp_emphbinaries_talkaspect_linear_defaultcleaning.pdf f3c.pdf
-cp ../results/rotation/NGC_2516/NGC_2516_rotation_vs_BpmRp_emphbinaries_talkaspect_linear_periodogram_match.pdf f3d.pdf
+cp ../results/rotation/NGC_2516/NGC_2516_rotation_vs_BpmRp_corehalosplit_talkaspect_linear_defaultcleaning.pdf f4a.pdf
+cp ../results/rotation/NGC_2516/NGC_2516_rotation_vs_BpmRp_corehalosplit_talkaspect_linear_periodogram_match.pdf f4b.pdf
+cp ../results/rotation/NGC_2516/NGC_2516_rotation_vs_BpmRp_emphbinaries_talkaspect_linear_defaultcleaning.pdf f5a.pdf
+cp ../results/rotation/NGC_2516/NGC_2516_rotation_vs_BpmRp_emphbinaries_talkaspect_linear_periodogram_match.pdf f5b.pdf
 
+cp ../results/physical_X_rotation/physical_X_rotation_fullfaint.pdf f6a.pdf
+cp ../results/physical_X_rotation/histogram_physical_X_rotation_fullfaint.pdf f6b.pdf
+cp ../results/rotation_X_positions/rotation_X_positions_approach2_defaultcleaning.pdf f6c.pdf
 
+cp ../results/lithium/lithiumEW_vs_BpmRp_xmatch_fullfaintkinematic_gaiaeso_galahdr3_corehalosplit_showkepfield_showpleiades_showpraesepe.pdf f7.pdf
 
-#cp ../results/skypositions/skypositions_x_rotn.pdf f4.pdf
-cp ../results/full_kinematics_X_rotation/full_kinematics_X_rotation_fullfaint_galactic.pdf f4.pdf
+cp ../results/rotation_X_lithium/rotation_vs_BpmRp_X_lithium_viridis_gaiaeso_galahdr3.pdf f8.pdf
 
-cp ../results/physical_X_rotation/physical_X_rotation_fullfaint.pdf f8a.pdf
-cp ../results/physical_X_rotation/histogram_physical_X_rotation_fullfaint.pdf f8b.pdf
+cp ../results/lumfunction_vs_position/lumfunction_vs_position.pdf f9.pdf
 
-cp ../results/lithium/lithiumEW_vs_BpmRp_xmatch_fullfaintkinematic_gaiaeso_galahdr3_corehalosplit_showkepfield_showpleiades_showpraesepe.pdf f5a.pdf
-cp ../results/rotation_X_lithium/rotation_vs_BpmRp_X_lithium_viridis_gaiaeso_galahdr3.pdf f5b.pdf
+cp ../results/litcomp_ngc2516/litcomp_ngc2516.pdf f10.pdf
 
-cp ../results/vtangential_projection/XYZ_with_orbit.pdf f11.pdf
-cp ../results/vtangential_projection/skymap_tangential_velocity_pmdec.pdf f12a.pdf
-cp ../results/vtangential_projection/skymap_tangential_velocity_pmra_cosdec.pdf f12b.pdf
+# appendix
+cp ../results/venn/venn.pdf f11.pdf
+cp ../results/lightcurves_rotators/lightcurves_rotators_color.pdf f12.pdf
+cp ../results/compstar_rotation/compstar_rotation_NGC_2516_vs_BpmRp_linear.pdf f13.pdf
 
-cp ../results/rotation_X_positions/rotation_X_positions_approach2_defaultcleaning.pdf f13.pdf
+cp ../results/vtangential_projection/skymap_tangential_velocity_pmdec.pdf f14a.pdf
+cp ../results/vtangential_projection/skymap_tangential_velocity_pmra_cosdec.pdf f14b.pdf
 
-cp ../results/lumfunction_vs_position/lumfunction_vs_position.pdf f15.pdf
-
-# supplementary
-cp ../results/rotation_X_RUWE/rotation_vs_BpmRp_X_RUWE_viridis_linear.pdf f6.pdf
-
-cp ../results/full_kinematics_X_rotation/full_kinematics_X_rotation_fullfaint_edr3_galactic.pdf f7.pdf
-
-cp ../results/compstar_rotation/compstar_rotation_NGC_2516_vs_BpmRp_linear.pdf f9.pdf
-
-cp ../results/venn/venn.pdf f10.pdf
-
-cp ../results/lightcurves_rotators/lightcurves_rotators_color.pdf f14.pdf
-cp ../results/litcomp_ngc2516/litcomp_ngc2516.pdf f16.pdf
-
+# cp ../results/cluster_membership/full_kinematics_bright_galactic.pdf f1.pdf
+# cp ../results/hr/hr_colorhalobyglat_Bp_m_Rp_show1937_fullfaint_edr3.pdf f2c.pdf
+# cp ../results/skypositions/skypositions_x_rotn.pdf f4.pdf
+# cp ../results/full_kinematics_X_rotation/full_kinematics_X_rotation_fullfaint_galactic.pdf f4.pdf
+# cp ../results/rotation_X_RUWE/rotation_vs_BpmRp_X_RUWE_viridis_linear.pdf f6.pdf
+# cp ../results/full_kinematics_X_rotation/full_kinematics_X_rotation_fullfaint_edr3_galactic.pdf f7.pdf
 # cp ../results/calc_backintegration_ngc2516/core_halo_to_ngc2516_separation.pdf f7a.pdf
 # cp ../results/calc_backintegration_ngc2516/core_halo_to_ngc2516_separation_fix_rvs.pdf f7b.pdf
-# 
 # cp ../results/glue_ngc2516_manual_rotator_viz/slow_rotators_are_lower_declination_and_closer_to_sun.png temp0.png
 # cp ../results/glue_ngc2516_manual_rotator_viz/fast_rotators_are_spread_throughout_core_and_halo.png temp1.png
